4de3bc9cc3997588bde944f1b1618cc2d10b644d
cole <f8cole@gmail.com>
2025-03-14 11:07:42 -0400

    add table

--- src/main.go
+++ src/main.go
@@ -1,25 +1,120 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
+type bookmark struct {
+	name string
+	path string
 }
 
-type model struct {
-	bookmarks map[string]string
+type Model struct {
+	bookmarks []bookmark
 	cursor    int
+	table     table.Model
 }
 
-func initialModel() model {
-	return model{
-		// Our to-do list is a grocery list
-		bookmarks: map[string]string{
-			"home": "C:\\Users\\f8col\\OneDrive\\Desktop\\Projects\\EBM",
-			"src":  "C:\\Users\\f8col\\OneDrive\\Desktop\\Projects\\EBM\\src",
-			"cmd":  "C:\\Users\\f8col\\OneDrive\\Desktop\\Projects\\EBM\\cmd",
-		},
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+func initialModel() Model {
+	f, err := os.Open("bm.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	reader := csv.NewReader(f)
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var bookmarks []bookmark
+	var rows []table.Row
+	for _, record := range records {
+		rows = append(rows, table.Row{
+			record[0],
+			record[1],
+		})
+	}
+
+	columns := []table.Column{
+		{Title: "Name", Width: 6},
+		{Title: "Path", Width: 90},
+	}
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(7),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	t.SetStyles(s)
+
+	return Model{
+		bookmarks: bookmarks,
+		cursor:    0,
+		table:     t,
+	}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc":
+			if m.table.Focused() {
+				m.table.Blur()
+			} else {
+				m.table.Focus()
+			}
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "enter":
+			return m, tea.Batch(
+				tea.Printf("Name: %s Path: %s", m.table.SelectedRow()[0], m.table.SelectedRow()[1]),
+			)
+
+		}
+	}
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
+}
+
+func (m Model) View() string {
+	return baseStyle.Render(m.table.View()) + "\n"
+}
+
+func main() {
+
+	m := initialModel()
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
 	}
 }
